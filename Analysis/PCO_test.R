3cf7f82c77b6287efb91441f099e59cb37441810
TGuillerme <guillert@tcd.ie>
2014-09-01 11:46:17 +0100

    STD draft functions working on empirical/simulated

--- Analysis/PCO_test.R
+++ Analysis/PCO_test.R
@@ -18,6 +18,8 @@ source('treeAge.R')
 #data
 tree<-read.tree('BDtree.tre')
 table<-read.dna('MorphoMat.phylip', as.character=TRUE, as.matrix=TRUE)
+#table<-read.dna('GappedMat.phylip', as.character=TRUE, as.matrix=TRUE)
+#table<-table[26:51,] #for a 75% missing data table
 tree.age<-treeAge(tree, max(tree$edge.length))
 plot(tree) ; axisPhylo()
 
@@ -214,12 +216,285 @@ pcoSlice<-function(tree, main="slice") {
     plot.pco(pcoSlice, clade.1[-which(is.na(clade.1)),], clade.2[-which(is.na(clade.2)),], main=main)
 }
 
-
 op<-par(mfrow=c(3, 2))
-pcoSlice(tree0.del, main="slice 0")
-pcoSlice(tree1.del, main="slice 1")
-pcoSlice(tree2.del, main="slice 2")
-pcoSlice(tree3.del, main="slice 3")
-pcoSlice(tree4.del, main="slice 4")
-pcoSlice(tree5.del, main="slice 5")
+pcoSlice(tree0.acc, main="slice 0")
+pcoSlice(tree1.acc, main="slice 1")
+pcoSlice(tree2.acc, main="slice 2")
+pcoSlice(tree3.acc, main="slice 3")
+pcoSlice(tree4.acc, main="slice 4")
+pcoSlice(tree5.acc, main="slice 5")
+par(op)
+
+
+
+
+#------------------------
+#Empirical data
+#------------------------
+#And now with real data
+Slater.table<-read.table("../Empirical_mammals/Matrices/2013-Slater-MEE-morpho.phylip", header=F, sep=" ", row.names=1) 
+Slater.tree<-read.nexus('../Empirical_mammals/Trees/2013-Slater-MEE.tre')
+plot(Slater.tree, show.tip.label=FALSE) ; axisPhylo()
+
+#Correct the table
+#Remove species only present in the table
+library(caper)
+missing.species<-comparative.data(Slater.tree, data.frame("species"=row.names(Slater.table), "dummy"=rnorm(nrow(Slater.table)), "dumb"=rnorm(nrow(Slater.table))), "species")$dropped$unmatched.rows
+suppressWarnings(table.tmp<-Slater.table[-c(which(row.names(Slater.table) == missing.species)),])
+bugged<-c("Abrocomidae","Aegialodon","Murtoilestes") #not sure why
+
+#Remove species with only missing data from the table
+missing<-which(duplicated(table.tmp)==TRUE)
+missing.na.species<-rownames(table.tmp[missing,])
+table.tmp<-table.tmp[-missing,]
+table<-as.matrix(table.tmp)
+
+#list of missing species (to drop in the tree)
+missing.data.species<-c(missing.species, missing.na.species, bugged) #not sure why?
+
+#TEST
+library(testthat)
+expect_equal((nrow(table.tmp)+length(missing.species)+ length(missing)), nrow(Slater.table))
+#expect_that(Ntip(Slater.tree), equals(nrow(table.tmp)))
+expect_is(table, "matrix")
+
+
+table<-table[-which(rownames(table) == bugged),] #for some reason Abrocomidae is not removed
+
+#Replacing "?" by NA's
+#for (character in 1:ncol(table)) {
+#    character.corrected<-as.factor(table[,character])
+#    wrong.levels<-levels(character.corrected)
+#    levels(character.corrected)<-c(NA, wrong.levels[-1])
+#    table[,character]<-character.corrected
+#}
+#if(any(grep("?", table.tmp))) {
+#    expect_true(anyNA(table))
+#} else {
+#    expect_false(anyNA(table))
+#}
+
+#68, 71
+stable<-table[-69,]
+table<-stable
+#stable<-table[-c(1, 68,71), ]
+eucl.table<-dist(table, method = "euclidean")
+pco<-pcoa(eucl.table)
+expect_is(pco, "pcoa")
+
+
+
+
+#Remove the species with only missing data from the tree
+tree.tmp<-drop.tip(Slater.tree, missing.data.species)
+#TEST
+expect_that(Ntip(tree.tmp), equals(nrow(table)))
+
+#Making the tree fully dichotomous
+tree<-multi2di(tree.tmp)
+expect_true(is.binary.tree(tree))
+#Adding a small branch length to the newly generated dichotomies (0.1*min(branch length))
+tree$edge.length[which(tree$edge.length == 0)]<-0.1*min(tree$edge.length[-which(tree$edge.length == 0)])
+expect_that(length(which(tree$edge.length == 0)), equals(0))
+#Adding tree nodes names
+
+#Calculate the tree age
+tree.age<-treeAge(tree)
+
+#plot the cleaned tree
+plot(tree, cex=0.5) ; axisPhylo()
+
+#Testing pco
+#Euclidian matrix
+eucl.table<-dist(table, method = "euclidean")
+
+#pcoa
+pco<-pcoa(eucl.table)
+
+
+#------------------------
+#Ancestral states
+#------------------------
+
+verbose=TRUE
+ancestral.list<-list()
+for (character in 1:ncol(table)) {
+    ancestral.list[[character]]<-ace(as.factor(table[,character]), tree, type = "d")
+    if(verbose==TRUE) {
+        message('.', appendLF=FALSE)
+    }
+}
+
+#------------------------
+#Creating the prob and state
+#------------------------
+nodes=tree$Nnode
+prob.matrix<-matrix(data=1, ncol=ncol(table), nrow=(nrow(table)+nodes))
+row.names(prob.matrix)<-c(row.names(table), paste("n",seq(1:nodes), sep=""))
+
+#Replacing the probability of each node (was 1) into the highest probability calculated in ancestral.list
+for (character in 1:ncol(table)) {
+    for (edge in 1:nodes) {
+        prob.matrix[(nrow(table)+edge),character]<-max(ancestral.list[[character]]$lik.anc[edge,])
+    }
+}
+
+#Creating the new characters state matrix for every tips and nodes
+state.matrix<-matrix(NA, ncol=ncol(table), nrow=(nrow(table)+nodes))
+row.names(state.matrix)<-c(row.names(table), paste("n",seq(1:nodes), sep=""))
+
+#Adding the character states for the tips (observed)
+state.matrix[1:nrow(table), 1:ncol(table)]<-as.matrix(table)
+
+#Adding the character states for the nodes (estimated)
+for (character in 1:ncol(table)) {
+    for (edge in 1:nodes) {
+        #Extracting the column name for each edge and character
+        max.prob<-which(ancestral.list[[character]]$lik.anc[edge,]==max(ancestral.list[[character]]$lik.anc[edge,]))[[1]]
+        state.matrix[(nrow(table)+edge),character]<-colnames(ancestral.list[[character]]$lik.anc)[max.prob] # -1 is to make the character start at 0 (which greps 1,2,3, etc... instead of 0,1,2, ...)
+    }
+}
+
+#Test all columns (no extra levels)
+for(character in 1:ncol(table)) {
+    expect_that(levels(as.factor(table[,character])) %in% as.vector(state.matrix[,character]), equals(rep(TRUE, length(levels(as.factor(table[,character]))))))
+}
+
+n.tips=1:Ntip(tree)
+n.nodes=(Ntip(tree)+1):(Ntip(tree)+Nnode(tree))
+
+expect_equal(Ntip(tree), nrow(table))
+expect_equal((Ntip(tree)+Nnode(tree)), nrow(state.matrix))
+expect_equal((nrow(state.matrix)-Ntip(tree)), Nnode(tree))
+expect_equal(length(n.tips), Ntip(tree))
+expect_equal(length(n.nodes), Nnode(tree))
+
+
+#Visual test
+for (character in 1:ncol(table)) {
+    co<-palette()
+    op<-par(mfrow=c(1, 2))
+    #Plot from the tree (observed)
+    plot(tree, label.offset=0.1, cex=0.5, main=paste("From tree ", character, sep="") , show.tip.label=FALSE)
+    tiplabels(pch=22, bg=co[as.numeric(as.factor(table[,character]))], cex=1)    
+    #Extract the state
+    anc.states<-vector()
+    for (row in 1:nrow(ancestral.list[[character]]$lik.anc)) {
+        anc.states[[row]]<-which(ancestral.list[[character]]$lik.anc[row,] == max(ancestral.list[[character]]$lik.anc[row,]))[[1]]
+    }
+    nodelabels(bg=co[as.numeric(anc.states)], cex=0.3)
+    #Plot from the state.matrix (estimated)
+    plot(tree, label.offset=0.1, cex=0.5, main=paste("From matrix ", character, sep=""), show.tip.label=FALSE)
+    character.levels<-levels(as.factor(state.matrix[,character]))
+    tiplabels(pch=22, bg=co[factor(state.matrix[n.tips,character], levels=character.levels)], cex=1)  #1:nrow(table)
+    nodelabels(bg=co[factor(state.matrix[n.nodes,character], levels=character.levels)], cex=0.3) #(nrow(table))+1):nrow(state.matrix)
+    par(op)
+    Sys.sleep(1)
+}
+
+
+
+tree.full<-tree
+tree.full$node.label<-row.names(state.matrix[(nrow(table)+1):nrow(state.matrix),])
+plot(tree.full, cex=0.5) ; axisPhylo() ; nodelabels(text=tree.full$node.label, cex=0.4)
+
+#------------------------
+#Global PCO
+#------------------------
+
+dist.matrix<-dist(state.matrix, method="euclidian")
+pco<-pcoa(dist.matrix) #Error in eigen(delta1) : infinite or missing values in 'x'
+#biplot(pco)
+
+
+#------------------------
+#PCO plot
+#------------------------
+#Adding edge names to the trees
+tree.full<-tree
+tree.full$node.label<-row.names(state.matrix[(nrow(table)+1):nrow(state.matrix),])
+
+clade1<-extract.clade(tree.full, 94)
+clade1<-drop.tip(clade1, extract.clade(tree.full, 105)$tip.label)
+clade1<-c(clade1$tip.label, clade1$node.label)
+clade2<-c(extract.clade(tree.full, 105)$tip.label, extract.clade(tree.full, 105)$node.label)
+
+pco.scores<-as.data.frame(pco$vectors)
+clade.col<-ncol(pco.scores)+1
+pco.scores[, clade.col]<-"NA"
+pco.scores[match(clade1, row.names(pco.scores)), clade.col]<-"non-eutheria"
+pco.scores[match(clade2, row.names(pco.scores)),clade.col]<-"eutheria"
+clade.1<-pco.scores[match(clade1, row.names(pco.scores)),1:2]
+clade.2<-pco.scores[match(clade2, row.names(pco.scores)),1:2]
+
+dev.new()
+plot.pco<-function(pco.scores ,clade.1 , clade.2, ...) {
+    library(grDevices)
+    plot(1,1, col="white", xlab="PC1", ylab="PC2", xlim=c(min(pco.scores[,1]), max(pco.scores[,1])), ylim=c(min(pco.scores[,2]), max(pco.scores[,2])), ...)
+    points(pco.scores[,1], pco.scores[,2], col=c("red", "blue", "green3")[as.factor(pco.scores[,clade.col])])
+    polygon(clade.1[chull(clade.1),], border="green3")
+    polygon(clade.2[chull(clade.2),], border="red")
+}
+
+plot.pco(pco.scores, clade.1, clade.2, main="with nodes")
+
+#------------------------
+#time slices
+#------------------------
+#Slicing plots
+source('sliceTree.R')
+
+#Six slices with ACCTRAN
+tree0.acc<-slice.tree(tree.full, age=0, method="ACCTRAN")
+tree1.acc<-slice.tree(tree.full, age=25, method="ACCTRAN")
+tree2.acc<-slice.tree(tree.full, age=50, method="ACCTRAN")
+tree3.acc<-slice.tree(tree.full, age=75, method="ACCTRAN")
+tree4.acc<-slice.tree(tree.full, age=100, method="ACCTRAN")
+tree5.acc<-slice.tree(tree.full, age=125, method="ACCTRAN")
+tree6.acc<-slice.tree(tree.full, age=150, method="ACCTRAN")
+tree7.acc<-slice.tree(tree.full, age=175, method="ACCTRAN")
+tree8.acc<-slice.tree(tree.full, age=200, method="ACCTRAN")
+
+#Six slices with DELTRAN
+tree0.del<-slice.tree(tree.full, age=0, method="DELTRAN")
+tree1.del<-slice.tree(tree.full, age=25, method="DELTRAN")
+tree2.del<-slice.tree(tree.full, age=50, method="DELTRAN")
+tree3.del<-slice.tree(tree.full, age=75, method="DELTRAN")
+tree4.del<-slice.tree(tree.full, age=100, method="DELTRAN")
+tree5.del<-slice.tree(tree.full, age=125, method="DELTRAN")
+tree6.del<-slice.tree(tree.full, age=150, method="DELTRAN")
+tree7.del<-slice.tree(tree.full, age=175, method="DELTRAN")
+tree8.del<-slice.tree(tree.full, age=200, method="DELTRAN")
+
+#make plot pco
+pcoSlice<-function(tree, main="slice") {
+    pcoSlice<-pco.scores[tree$tip.label,]
+    clade.1<-pcoSlice[match(clade1, row.names(pcoSlice)),1:2]
+    clade.2<-pcoSlice[match(clade2, row.names(pcoSlice)),1:2]
+    plot.pco(pcoSlice, clade.1[-which(is.na(clade.1)),], clade.2[-which(is.na(clade.2)),], main=main)
+}
+
+op<-par(mfrow=c(3, 3))
+pcoSlice(tree0.del, main="0 Mya")
+pcoSlice(tree1.del, main="25 Mya")
+pcoSlice(tree2.del, main="50 Mya")
+pcoSlice(tree3.del, main="75 Mya")
+pcoSlice(tree4.del, main="100 Mya")
+pcoSlice(tree5.del, main="125 Mya")
+pcoSlice(tree6.del, main="150 Mya")
+pcoSlice(tree7.del, main="175 Mya")
+pcoSlice(tree8.del, main="200 Mya")
+par(op)
+
+
+op<-par(mfrow=c(3, 3))
+pcoSlice(tree0.acc, main="0 Mya")
+pcoSlice(tree1.acc, main="25 Mya")
+pcoSlice(tree2.acc, main="50 Mya")
+pcoSlice(tree3.acc, main="75 Mya")
+pcoSlice(tree4.acc, main="100 Mya")
+pcoSlice(tree5.acc, main="125 Mya")
+pcoSlice(tree6.acc, main="150 Mya")
+pcoSlice(tree7.acc, main="175 Mya")
+pcoSlice(tree8.acc, main="200 Mya")
 par(op)
