6f51fd1c2f692af36a5ee1f547ec566d6d6d43ed
TGuillerme <guillert@tcd.ie>
2014-10-21 18:21:10 +0100

    Fixed bug with old anc.matrix

--- Functions/test/test.anc.unc.R
+++ Functions/test/test.anc.unc.R
@@ -10,19 +10,19 @@ set.seed(10)
 #prob matrix with only two cells == 1
 matrix.prob<-data.frame(row.names=tree$tip.label, "char1"=rnorm(10), "char2"=rnorm(10))
 matrix.prob=matrix.prob^2 ; matrix.prob[,1]<-matrix.prob[,1]/max(matrix.prob[,1]) ; matrix.prob[,2]<-matrix.prob[,2]/max(matrix.prob[,2])
-anc.mat<-list("state"=matrix.state, "prob"=matrix.prob)
+anc.mat<-list("state"=matrix.state, "prob"=matrix.prob, "rate"=NA)
 test<-anc.unc(anc.mat, threshold=1)
 
 #test
 #anc.mat
 expect_is(anc.mat, 'list') ; message('.', appendLF=FALSE)
-expect_that(length(anc.mat), equals(2)) ; message('.', appendLF=FALSE)
-expect_that(names(anc.mat), equals(c("state", "prob"))) ; message('.', appendLF=FALSE)
+expect_that(length(anc.mat), equals(3)) ; message('.', appendLF=FALSE)
+expect_that(names(anc.mat), equals(c("state", "prob", "rate"))) ; message('.', appendLF=FALSE)
 
 #object
 expect_is(test, 'list') ; message('.', appendLF=FALSE)
-expect_that(length(test), equals(2)) ; message('.', appendLF=FALSE)
-expect_that(names(test), equals(c("state", "prob"))) ; message('.', appendLF=FALSE)
+expect_that(length(test), equals(3)) ; message('.', appendLF=FALSE)
+expect_that(names(test), equals(c("state", "prob", "rate"))) ; message('.', appendLF=FALSE)
 
 #only two cells in the right threshold
 expect_equal(which(is.numeric(suppressWarnings(as.numeric(test$state[,1])))), 1) ; message('.', appendLF=FALSE)
