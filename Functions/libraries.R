c174ad2225d39e9bfbdc479dc16a58d63ac039fc
TGuillerme <guillert@tcd.ie>
2014-11-21 10:02:15 +0000

    Updated package manager

--- Functions/libraries.R
+++ Functions/libraries.R
@@ -1,3 +1,9 @@
+#Installing packages if necessary
+list.of.packages <- c("diversitree", "Claddis", "paleotree", "strap", "ape", "caper", "phytools", "vegan", "grDevices", "testthat")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#Load the packages
 suppressMessages({
     library(diversitree)
     library(Claddis)
@@ -5,7 +11,6 @@ suppressMessages({
     library(strap)
     library(ape)
     library(caper)
-    library(paleotree)
     library(phytools)
     library(vegan)
     library(grDevices)
