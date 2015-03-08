b20f20c7b0b7897dc97edc55b9e06ee391057513
TGuillerme <guillert@tcd.ie>
2015-03-08 14:16:16 +0000

    Data setup pipeline

---
+++ Analysis/Data_setup.R
@@ -0,0 +1,61 @@
+#Script for doing ancestral states reconstruction and distance matrix from an input nexus file
+
+#Setwd
+if(length(grep("TGuillerme", getwd()))) {
+    setwd('~/PhD/Projects/SpatioTemporal_Disparity/Analysis')
+} else {
+    warning("You might have to change the directory!")
+}
+if(length(grep("SpatioTemporal_Disparity/Analysis", getwd()))==0) {
+    if(length(grep("SpatioTemporal_Disparity-master/Analysis", getwd()))==0) {
+        stop("Wrong directory!\nThe current directory must be:\nSpatioTemporal_Disparity/Analysis/ OR SpatioTemporal_Disparity-master/Analysis/\nYou can clone the whole repository from:\nhttps://github.com/TGuillerme/SpatioTemporal_Disparity")
+    }
+}
+
+#Load the functions and the packages
+source("functions.R")
+
+###################
+#Reading the files
+###################
+
+#Selecting the file
+file_matrix<-"../Data/2014-Beck-ProcB-matrix-morpho.nex"
+file_tree<-"../Data/2014-Beck-ProcB-TEM.tre"
+
+#matrix
+Nexus_data<-ReadMorphNexus(file_matrix)
+Nexus_matrix<-Nexus_data$matrix
+#tree
+Tree_data<-read.nexus(file_tree)
+
+######################################
+#Cleaning the matrices and the trees
+######################################
+
+#Remove species with only missing data before hand
+if (any(apply(as.matrix(Nexus_matrix), 1, function(x) levels(as.factor((x)))) == "?")) {
+    Nexus_matrix<-Nexus_matrix[-c(as.vector(which(apply(as.matrix(Nexus_matrix), 1, function(x) levels(as.factor(x))) == "?"))),]
+}
+
+#Cleaning the tree and the table
+tree<-clean.tree(Tree_data, Nexus_matrix)
+table<-clean.table(Nexus_matrix, Tree_data)
+
+#Forcing the tree to be binary
+tree<-bin.tree(tree)
+
+#Adding node labels to the tree
+tree$node.label<-paste("n",seq(1:Nnode(tree)), sep="")
+
+####################################
+#Ancestral states reconstruction
+####################################
+
+
+
+####################################
+#Distance matrix
+####################################
+
+#dist.data <- MorphDistMatrix(nexus.data)
