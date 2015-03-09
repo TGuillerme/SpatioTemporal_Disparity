f5e7e282e038608034ab865f5ec8eaebf6a4a88c
TGuillerme <guillert@tcd.ie>
2015-03-09 11:27:06 +0000

    v1.0 synchronised with anc.state function

--- Functions/anc.unc.R
+++ Functions/anc.unc.R
@@ -2,48 +2,54 @@
 #Ancestral uncertainty
 ##########################
 #Transforms the ancestral state matrix by excluding all infered states below a certain threshold.
-#v0.1.1
-#Update:
-#Fixed sanitizing to match with the new anc.matrix type (+rate)
+#1.0
+#Update: Fixed sanitizing to match with the new anc.matrix type (+rate)
+#Update: Synchronized with the anc.state function v1.0
 ##########################
 #SYNTAX :
 #<anc.matrix> the ancestral matrix (a list of states and probabilities returned from anc.state)
 #<threshold> the probability threshold (a value between 0.5 and 1)
+#<missing> any missing character symbol to use
 ##########################
 #----
 #guillert(at)tcd.ie 29/09/2014
 ##########################
 
-anc.unc<-function(anc.matrix, threshold=0.5) {
+anc.unc<-function(anc.matrix, threshold=0.5, missing="?") {
     #SANITIZING
     #anc.matrix
-    check.class(anc.matrix, "list", " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.")
-    check.length(anc.matrix, 3, " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.")
-    if(names(anc.matrix)[1] != "state") {
-        stop(as.character(substitute(anc.matrix)), " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.", call.=FALSE)
-    } 
-    if(names(anc.matrix)[2] != "prob") {
-        stop(as.character(substitute(anc.matrix)), " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.", call.=FALSE)
+    #class
+    check.class(anc.matrix, "list", " must be a list containing at least two elements: \'state\' and \'prob\'.\nUse anc.state() function for proper formating.")
+    #length
+    if(length(anc.matrix ) < 2) {
+        stop("anc.matrix must be a list containing at least two elements: \'state\' and \'prob\'.\nUse anc.state() function for proper formating.")
     }
-    if(names(anc.matrix)[3] != "rate") {
-        stop(as.character(substitute(anc.matrix)), " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.", call.=FALSE)
+    #check names
+    if(length(grep("state" ,names(anc.matrix))) == 0) {
+        stop("anc.matrix must be a list containing at least two elements: \'state\' and \'prob\'.\nUse anc.state() function for proper formating.")
+    }
+    if(length(grep("prob", names(anc.matrix))) == 0) {
+        stop("anc.matrix must be a list containing at least two elements: \'state\' and \'prob\'.\nUse anc.state() function for proper formating.")
     }
 
     #threshold
     check.class(threshold, 'numeric', ' must be a numerical value between 0.5 and 1.')
     check.length(threshold, 1, ' must be a numerical value between 0.5 and 1.')
     if(threshold < 0.5) {
-        stop('threshold must be a numerical value between 0.5 and 1.', call.=FALSE) 
+        stop('threshold must be a numerical value between 0.5 and 1.') 
     }
     if(threshold > 1) {
-        stop('threshold must be a numerical value between 0.5 and 1.', call.=FALSE) 
+        stop('threshold must be a numerical value between 0.5 and 1.') 
     }
+
+    #missing
+    check.length(missing, 1, ' must be a single symbol.')
 
     #REPLACING CHARACTER STATES BY "?" IN THE MATRIX IF STATE PROBABILITY < threshold
     for (character in 1:ncol(anc.matrix$prob)) {
         for (taxa in 1:nrow(anc.matrix$prob)) {
             if(anc.matrix$prob[taxa, character] < threshold) {
-                anc.matrix$state[taxa, character] <- "?"
+                anc.matrix$state[taxa, character] <- missing
             }
         }
     }
