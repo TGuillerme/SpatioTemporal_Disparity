7ee89096849a51b6171e9b36aebd9b040ce9d892
TGuillerme <guillert@tcd.ie>
2015-03-10 15:19:26 +0000

    v1.0 for slice.pco and slice.tree
    Updated methods (random) and standardised slice.pco output.

---
+++ Functions/slice.pco.R
@@ -0,0 +1,112 @@
+##########################
+#slice.pco
+##########################
+#Select the number of taxa and pco data per slice.
+#v0.1
+##########################
+#SYNTAX :
+#<pco_data> the pco data to split in bins.
+#<tree> a 'phylo' object. The tree must be dated.
+#<slices> slices ages. Can be either a single value (for a single slice) or a series of values (for multiple slices)
+#<method> the slicing method (what becomes of the sliced branches): can be 'random' (default), 'acctran', 'deltran' or 'proximity'.
+#<FAD_LAD> a data.frame containing the first and last apparition datums. If none is provided, or if taxa are missing, taxa are assumed to have the same FAD and LAD.
+##########################
+#Method details: the slicing methods are the method of the edge to chose when cutting through a branch. At any point of the branch cut, the different method picks either the data of the parent node or one of the offspring node or tip.
+#random: randomly chose between parent and offspring (default);
+#acctran: always chose offspring;
+#deltran: always chose parent;
+#prozimity: chose between the parent or the offspring based on branch length. If the cut is equal to more than half the branch length, the offspring is chosen, else the parent.
+#----
+#guillert(at)tcd.ie 10/03/2015
+##########################
+
+slice.pco<-function(pco_data, tree, slices, method="random", FAD_LAD) {
+
+    #SANITIZING
+
+    #pco
+    check.class(pco_data, 'matrix', ' must be a pco scores matrix.')
+
+    #tree
+    check.class(tree, 'phylo', ' must be a phylo object.')
+    #the tree must be dated
+    if(length(tree$root.time)==0){
+        stop("Tree must be a dated tree with $root.time.")
+    }
+    #the tree must have node labels
+    if(is.null(tree$node.label)) {
+        stop('The tree must have node label names.')
+    }
+
+    #slices
+    check.class(slices, 'numeric', ' must be numeric.')
+    #slice cannot be older than the root age
+    if(any(slices > tree$root.time)) {
+        stop("Slices cannot be older than the tree's root age.")
+    }
+
+    #method
+    check.class(method, 'character', " must be either \'random\', \'acctran\', \'deltran\' or \'proximity\'.")
+    check.length(method, 1, " must be either \'random\', \'acctran\', \'deltran\' or \'proximity\'.", errorif=FALSE)
+    METHODS<-c("random", "acctran", "deltran", "proximity")
+    if(!any(method == METHODS)) {
+        stop("method must be either \'random\', \'acctran\', \'deltran\' or \'proximity\'.")
+    }
+
+    #FAD_LAD
+    if(missing(FAD_LAD)) {
+        #Create the FAD_LAD table
+        FAD_LAD<-data.frame("FAD"=tree.age(tree)[1:Ntip(tree),1], "LAD"=tree.age(tree)[1:Ntip(tree),1], row.names=tree.age(tree)[1:Ntip(tree),2])
+        message("No FAD_LAD table has been provided so every tip is assumed to bin single points in time.")
+    } else {
+        #Check if the FAD_LAD contains all taxa
+        if(any(tree$tip.label %in% rownames(FAD_LAD) == FALSE)) {
+            message("Some tips have FAD/LAD and are assumed to bin single points in time.")
+            #If not generate the FAD_LAD for the missing taxa
+            missing_FADLAD<-which(is.na(match(tree$tip.label, rownames(FAD_LAD))))
+            add_FAD_LAD<-data.frame(tree.age(tree)[missing_FADLAD,1], tree.age(tree)[missing_FADLAD,1], row.names=tree.age(tree)[missing_FADLAD,2])
+            colnames(add_FAD_LAD)<-colnames(FAD_LAD)
+            FAD_LAD<-rbind(FAD_LAD, add_FAD_LAD)
+        }
+    }
+
+    #SLICING THE TREE
+    #Number of slices
+    n_slices<-length(slices)
+
+    #ages of tips/nodes + FAD/LAD
+    ages_tree_FAD<-tree.age(tree)
+    ages_tree_LAD<-tree.age(tree)
+    #Change the age if FAD or LAD are higher/lower than the age of the tip
+    for(tip in 1:nrow(FAD_LAD)) {
+        #Replace age of the tip if FAD is higher
+        if(FAD_LAD[tip,1] > ages_tree_FAD$ages[which(ages_tree_FAD$edges == rownames(FAD_LAD)[tip])]) {
+            ages_tree_FAD$ages[which(ages_tree_FAD$edges == rownames(FAD_LAD)[tip])]<-FAD_LAD[tip,1]
+        }
+        #Replace age of the tip if LAD is lower
+        if(FAD_LAD[tip,2] < ages_tree_LAD$ages[which(ages_tree_LAD$edges == rownames(FAD_LAD)[tip])]) {
+            ages_tree_LAD$ages[which(ages_tree_LAD$edges == rownames(FAD_LAD)[tip])]<-FAD_LAD[tip,2]
+        }
+    }
+
+    #Slicing the tree
+    slice_list<-NULL
+    slice_list<-list()
+
+    for (slice in 1:n_slices) {
+        #subtree
+        sub_tree<-slice.tree(tree, slices[slice], method, FAD=ages_tree_FAD, LAD=ages_tree_LAD)
+        #subtaxa list
+        sub_taxa<-sub_tree$tip.label
+        #subpco scores
+        sub_pco<-pco_data[sub_taxa,]
+        #storing the results
+        slice_list[[slice]]<-sub_pco
+    }
+
+    #naming the slices
+    names(slice_list)<-slices
+
+    return(slice_list)
+#End   
+}
--- Functions/slice.tree.R
+++ Functions/slice.tree.R
@@ -2,22 +2,30 @@
 #slice.tree
 ##########################
 #Slices a tree given a specific age
-#Modyfied timeSliceTree function from David Bapst (paleotrree)
-#v0.3
+#Modified from paleotree::timeSliceTree
+#v1.0
 #Update: added RATES method
 #Update: changed the RATES method into PROXIMITY
-#To do: change the slicing methods
+#Update: added possibility of adding FAD_LAD data
+#Update: added random method
 ##########################
 #SYNTAX :
 #<tree> a 'phylo' object
 #<age> where to slice the tree
-#<method> the slicing method (what becomes of the sliced branches): can be acctran, deltran, proximity or random
+#<method> the slicing method (what becomes of the sliced branches): can be random, acctran, deltran or proximity.
+#<FAD> optional First Apparition Datum data (tree.age format)
+#<LAD> optional Last Apparition Datum data (tree.age format)
 ##########################
+#Method details: the slicing methods are the method of the edge to chose when cutting through a branch. At any point of the branch cut, the different method picks either the data of the parent node or one of the offspring node or tip.
+#random: randomly chose between parent and offspring (default);
+#acctran: always chose offspring;
+#deltran: always chose parent;
+#prozimity: chose between the parent or the offspring based on branch length. If the cut is equal to more than half the branch length, the offspring is chosen, else the parent.
 #----
-#guillert(at)tcd.ie 20/10/2014
+#guillert(at)tcd.ie 10/03/2015
 ##########################
 
-slice.tree<-function(tree, age, method, anc.matrix) {
+slice.tree<-function(tree, age, method, FAD, LAD) {
 
     #SANITIZING
     #tree
@@ -30,41 +38,24 @@ slice.tree<-function(tree, age, method, anc.matrix) {
     #age
     check.class(age, 'numeric', " must be numeric.")
     #age must be at least higher than the root age
-    if(age > max(tree.age(tree)[,1])) {
-        stop('Age is lower than the root age!')
+    if(age > tree$root.time) {
+        stop("age cannot be older than the tree's root age.")
     }
 
     #method
-    check.class(method, 'character', " must be \'ACCTRAN\', \'DELTRAN\' or \'PROXIMITY\'.")
-    if(method != "ACCTRAN") {
-        if(method != "DELTRAN") {
-            if(method != "RATES") {
-                if(method != "PROXIMITY") {
-                    stop(as.character(substitute(method)), " must be \'ACCTRAN\', \'DELTRAN\' or \'PROXIMITY\'." , call.=FALSE)
-                }
-            }
-        }
+    check.class(method, 'character', " must be either \'random\', \'acctran\', \'deltran\' or \'proximity\'.")
+    check.length(method, 1, " must be either \'random\', \'acctran\', \'deltran\' or \'proximity\'.", errorif=FALSE)
+    METHODS<-c("random", "acctran", "deltran", "proximity")
+    if(!any(method == METHODS)) {
+        stop("method must be either \'random\', \'acctran\', \'deltran\' or \'proximity\'.")
     }
 
-    #anc.matrix
-    if(method == "RATES") {
-        if(missing(anc.matrix)) {
-            stop("Missing ancestral matrix for the \"RATES\" method.")
-        } else {
-            check.class(anc.matrix, "list", " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.")
-            check.length(anc.matrix, 3, " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.")
-            if(names(anc.matrix)[1] != "state") {
-                stop(as.character(substitute(anc.matrix)), " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.", call.=FALSE)
-            } 
-            if(names(anc.matrix)[2] != "prob") {
-                stop(as.character(substitute(anc.matrix)), " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.", call.=FALSE)
-            }
-            if(names(anc.matrix)[3] != "rate") {
-                stop(as.character(substitute(anc.matrix)), " must be a list from anc.state containing three elements: \'state\', \'prob\' and \'rate\'.", call.=FALSE)
-            }
-            rate<-anc.matrix$rate
-            prob<-anc.matrix$prob
-        }
+    #FAD/LAD
+    if(missing(FAD)) {
+        FAD<-tree.age(tree)
+    }
+    if(missing(LAD)) {
+        LAD<-tree.age(tree)
     }
 
     #SLICING A TREE
@@ -77,7 +68,7 @@ slice.tree<-function(tree, age, method, anc.matrix) {
     )
     #Error with trees with two taxa
     if(Ntip(tree_slice) < 3) {
-        stop('To few taxa for the tree slice at age ', age, '!', call.=FALSE)
+        stop('To few taxa for the tree slice at age ', age, '!')
     }
 
     #Selecting the tips
@@ -90,20 +81,42 @@ slice.tree<-function(tree, age, method, anc.matrix) {
     for (tip in 1:Ntip(tree_slice)) {
 
         #Check if the tree is sliced at the exact age of a tip (e.g. time=0)
-        if(tree_age[which(tree_age[,2]==tips[tip]),1] != age) {
-            if(method == "DELTRAN") {
-                tree_sliced$tip.label[tip]<-slice.tree_DELTRAN(tree, tips[tip], tree_slice)
+        if(tree_age[which(tree_age[,2]==tips[tip]),1] == age) {
+            #Save the tip
+            tree_slice$tip.label[tip]<-tree_slice$tip.label[tip]
+
+        } else {
+
+            #Check if the age of the tip is in between the FAD/LAD
+            if(FAD[which(FAD[,2]==tips[tip]),1] >= age & LAD[which(LAD[,2]==tips[tip]),1] <= age) {
+                #Save the tip
+                tree_slice$tip.label[tip]<-tree_slice$tip.label[tip]
+
+            } else {
+
+                #Chose the tip/node following the given method
+                if(method == "random") {
+                    selected_method<-sample(c("deltran", "acctran"), 1)
+                } else {
+                    selected_method<-method
+                }
+
+                if(selected_method == "deltran") {
+                    #Parent
+                    tree_sliced$tip.label[tip]<-slice.tree_DELTRAN(tree, tips[tip], tree_slice)
+                }
+
+                if(selected_method == "acctran") {
+                    #Offspring
+                    tree_sliced$tip.label[tip]<-slice.tree_ACCTRAN(tree, tips[tip], tree_slice)
+                }
+
+                if(selected_method == "proximity") {
+                    #Closest
+                    tree_sliced$tip.label[tip]<-slice.tree_PROXIMITY(tree, tips[tip], tree_slice)
+                }              
             }
-            if(method == "ACCTRAN") {
-                tree_sliced$tip.label[tip]<-slice.tree_ACCTRAN(tree, tips[tip], tree_slice)
-            }
-            if(method == "PROXIMITY") {
-                tree_sliced$tip.label[tip]<-slice.tree_PROXIMITY(tree, tips[tip], tree_slice)
-            }
-            if(method == "RATES") {
-                stop("TO DO!")
-            }
-        }
+        } 
     }
 
     return(tree_sliced)
