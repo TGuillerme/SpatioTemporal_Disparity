14ae60de99b75248a714a25abbb877ba4efed567
TGuillerme <guillert@tcd.ie>
2015-03-18 11:31:17 +0000

    Added states count utility function

--- Functions/sanitizing.R
+++ Functions/sanitizing.R
@@ -88,3 +88,23 @@ bin.tree<-function(tree){
     }
     return(tree)
 }
+
+#Counting the number of states per characters
+states.count<-function(character) {
+    #Isolate the states
+    states<-levels(as.factor(character))
+    #Check if multi states
+    if(length(grep("&", states)) > 0) {
+        #Isolating the multi states
+        multi_states<-states[grep("&", states)]
+        multi_states<-as.factor(unlist(strsplit(multi_states, split="&")))
+        #Removing the multi states from the states list
+        states<-states[-grep("&", states)]
+        #Check if any of the multi states is not yet present in the states list
+        if(any(is.na(match(levels(multi_states), states)))) {
+            states<-c(states, multi_states[which(is.na(match(levels(multi_states), states)))])
+        }
+    }
+    #Count the number of states
+    return(length(states))
+}
