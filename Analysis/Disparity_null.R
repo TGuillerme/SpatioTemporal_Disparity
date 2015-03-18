e2438117632e8bc3a087108771bcf4193d6e2b88
TGuillerme <guillert@tcd.ie>
2015-03-18 17:35:34 +0000

    Null disparity analysis (untested)

--- Analysis/Disparity_null.R
+++ Analysis/Disparity_null.R
@@ -69,44 +69,63 @@ FADLAD<-read.csv(paste(data_path, chain_name, "_FADLAD.csv", sep=""), row.names=
 #Ancestral states reconstruction files
 ######################
 
-load(paste(data_path, chain_name, "/", chain_name, "_ancestral_states-claddis.Rda", sep="")) #anc_states
+#load(paste(data_path, chain_name, "/", chain_name, "_ancestral_states-claddis.Rda", sep="")) #anc_states
 
 ######################
 #Distance matrices
 ######################
 
 load(paste(data_path, chain_name, "/", chain_name, "_distance-tips.Rda", sep="")) #dist_tips
-load(paste(data_path, chain_name, "/", chain_name, "_distance-nodes.Rda", sep="")) #dist_nodes
-load(paste(data_path, chain_name, "/", chain_name, "_distance-nodes95.Rda", sep="")) #dist_nodes95
+load(paste(data_path, chain_name, "/", chain_name, "_null_mat_rand_dist.Rda", sep="")) #null_mat_rand_dist
+load(paste(data_path, chain_name, "/", chain_name, "_null_mat_simchar_dist.Rda", sep="")) #null_mat_simchar_dist
 
+#Extracting a list of max.dist.matrix
+max_null_rand<-list()
+max_null_simc<-list()
+for (replicate in 1:length(null_mat_rand_dist)) {
+    max_null_rand[[replicate]]<-null_mat_rand_dist[[replicate]]$max.dist.matrix
+    max_null_simc[[replicate]]<-null_mat_simchar_dist[[replicate]]$max.dist.matrix
+}
 #Remove the inapplicable characters
 trimmed_max_data_tips<-TrimMorphDistMatrix(dist_tips$max.dist.matrix)
-trimmed_max_data_nodes<-TrimMorphDistMatrix(dist_nodes$max.dist.matrix)
-trimmed_max_data_nodes95<-TrimMorphDistMatrix(dist_nodes95$max.dist.matrix)
+trimmed_max_null_rand<-lapply(max_null_rand, TrimMorphDistMatrix)
+trimmed_max_null_simc<-lapply(max_null_simc, TrimMorphDistMatrix)
+
+#Extracting list of removed taxa
+max_null_rand_rm<-list()
+max_null_simc_rm<-list()
+for (replicate in 1:length(null_mat_rand_dist)) {
+    max_null_rand_rm[[replicate]]<-trimmed_max_null_rand[[replicate]]$removed.taxa
+    max_null_simc_rm[[replicate]]<-trimmed_max_null_simc[[replicate]]$removed.taxa
+}
+
 #Remove the dropped taxa from the tree
 tree_tips<-drop.tip(tree, trimmed_max_data_tips$removed.taxa)
-tree_nodes<-drop.tip(tree, trimmed_max_data_nodes$removed.taxa)
-tree_nodes95<-drop.tip(tree, trimmed_max_data_nodes95$removed.taxa)
-#Remove the eventual inapplicable nodes
-trimmed_max_data_nodes$dist.matrix<-trimmed_max_data_nodes$dist.matrix[c(tree_nodes$tip.label, tree_nodes$node.label),c(tree_nodes$tip.label, tree_nodes$node.label)]
-trimmed_max_data_nodes95$dist.matrix<-trimmed_max_data_nodes95$dist.matrix[c(tree_nodes$tip.label, tree_nodes$node.label),c(tree_nodes95$tip.label, tree_nodes95$node.label)]
+tree_rand<-lapply(max_nul_rand_rm, function(x) drop.tip(tree, x))
+tree_simc<-lapply(max_nul_simc_rm, function(x) drop.tip(tree, x))
 
 #List of trees
-trees<-list("tips"=tree_tips, "nodes"=tree_nodes, "nodes95"=tree_nodes95)
+#trees<-list("tips"=tree_tips, "nodes"=tree_nodes, "nodes95"=tree_nodes95)
 
 ######################
 #PCO
 ######################
 
 pco_data_tips<-cmdscale(trimmed_max_data_tips$dist.matrix, k=nrow(trimmed_max_data_tips$dist.matrix) - 1, add=T)$points
-pco_data_nodes<-cmdscale(trimmed_max_data_nodes$dist.matrix, k=nrow(trimmed_max_data_nodes$dist.matrix) - 1, add=T)$points
-pco_data_nodes95<-cmdscale(trimmed_max_data_nodes95$dist.matrix, k=nrow(trimmed_max_data_nodes95$dist.matrix) - 1, add=T)$points
+pco_null_rand<-lapply(trimmed_max_null_rand, cmdscale, k=nrow(trimmed_max_null_rand) - 1, add=T)
+pco_null_simc<-lapply(trimmed_max_null_simc, cmdscale, k=nrow(trimmed_max_null_simc) - 1, add=T)
+
+#Isolate only the pco scores for the lists
+for (replicate in 1:length(null_mat_rand_dist)) {
+    pco_null_rand[[replicate]]<-pco_null_rand[[replicate]]$points
+    pco_null_simc[[replicate]]<-pco_null_simc[[replicate]]$points
+}
 
 #Storing as a list
-pco_data<-list("tips"=pco_data_tips, "nodes"=pco_data_nodes, "nodes95"=pco_data_nodes95)
+#pco_data<-list("tips"=pco_data_tips, "nodes"=pco_data_nodes, "nodes95"=pco_data_nodes95)
 
 ######################
-#Disparity
+#Disparity - per interval only for now - centroid only for now
 ######################
 
 #Calculating the rarefaction
@@ -114,74 +133,19 @@ pco_data<-list("tips"=pco_data_tips, "nodes"=pco_data_nodes, "nodes95"=pco_data_
 
 #Generating the different intervals PCOs
 pco_int_tips<-int.pco(pco_data_tips, tree_tips, int_breaks, include.nodes=FALSE, FAD_LAD=FADLAD)
-pco_int_nodes<-int.pco(pco_data_nodes, tree_nodes, int_breaks, include.nodes=TRUE, FAD_LAD=FADLAD)
-pco_int_nodes95<-int.pco(pco_data_nodes95, tree_nodes95, int_breaks, include.nodes=TRUE, FAD_LAD=FADLAD)
+
+pco_int_null_rand<-list()
+pco_int_null_simc<-list()
+for (replicate in 1:length(null_mat_rand_dist)) {
+    pco_int_null_rand[[replicate]]<-int.pco(pco_int_null_rand[[replicate]], tree_rand[[replicate]], int_breaks, include.nodes=FALSE, FAD_LAD=FADLAD)
+    pco_int_null_simc[[replicate]]<-int.pco(pco_int_null_simc[[replicate]], tree_simc[[replicate]], int_breaks, include.nodes=FALSE, FAD_LAD=FADLAD)
+}
 
 #Calculating the disparity per intervals
-disp_int_tips<-time.disparity(pco_int_tips, verbose=TRUE)
-#disp_int_tips_95axis<-time.disparity(pco_int_tips, verbose=TRUE, rm.last.axis=TRUE)
-disp_int_nodes<-time.disparity(pco_int_nodes, verbose=TRUE)
-#disp_int_nodes_95axis<-time.disparity(pco_int_nodes, verbose=TRUE, rm.last.axis=TRUE)
-disp_int_nodes95<-time.disparity(pco_int_nodes95, verbose=TRUE)
-#disp_int_nodes95_95axis<-time.disparity(pco_int_nodes95, verbose=TRUE, rm.last.axis=TRUE)
+disp_int_tips<-time.disparity(pco_int_tips, method="centroid", verbose=TRUE)
+disp_int_null_rand<-lapply(pco_int_null_rand, time.disparity, , method="centroid", verbose=TRUE)
+disp_int_null_simc<-lapply(pco_int_null_simc, time.disparity, , method="centroid", verbose=TRUE)
 
-#Generating the different PCO slices
-#methods list
-methods=c("random", "acctran", "deltran", "proximity")
-#nodes
-pco_slices_nodes<-list()
-for (type in 1:length(methods)) {
-    pco_slices_nodes[[type]]<-slice.pco(pco_data_nodes, tree_nodes, slices, method=methods[[type]], FAD_LAD=FADLAD, verbose=TRUE) 
-}
-names(pco_slices_nodes)<-methods
+#Combining the list data together
 
-#nodes95
-pco_slices_nodes95<-list()
-for (type in 1:length(methods)) {
-    pco_slices_nodes95[[type]]<-slice.pco(pco_data_nodes95, tree_nodes95, slices, method=methods[[type]], FAD_LAD=FADLAD, verbose=TRUE)
-}
-names(pco_slices_nodes95)<-paste(methods, "95", sep="")
-
-#Calculating the disparity per interval per list
-#nodes
-disp_slices_nodes<-list()
-for (type in 1:length(methods)) {
-    disp_slices_nodes[[type]]<-time.disparity(pco_slices_nodes[[type]], verbose=TRUE)
-}
-names(disp_slices_nodes)<-names(pco_slices_nodes)
-
-#nodes95
-disp_slices_nodes95<-list()
-for (type in 1:length(methods)) {
-    disp_slices_nodes95[[type]]<-time.disparity(pco_slices_nodes95[[type]], verbose=TRUE)
-}
-names(disp_slices_nodes95)<-names(pco_slices_nodes95)
-
-
-
-############################
-#Maximum disparity
-############################
-#Extract the number of states per characters
-states.count<-function(character) {
-    #Isolate the states
-    states<-levels(as.factor(character))
-    #Check if multi states
-    if(length(grep("&", states)) > 0) {
-        #Isolating the multi states
-        multi_states<-states[grep("&", states)]
-        multi_states<-as.factor(unlist(strsplit(multi_states, split="&")))
-        #Removing the multi states from the states list
-        states<-states[-grep("&", states)]
-        #Check if any of the multi states is not yet present in the states list
-        if(any(is.na(match(levels(multi_states), states)))) {
-            states<-c(states, multi_states[which(is.na(match(levels(multi_states), states)))])
-        }
-    }
-    #Count the number of states
-    return(length(states))
-}
-
-states_list<-apply(Nexus_data$matrix, 2, states.count)
-max_sp<-prod(states_list) #really high number!
-
+#Plotting the different models
