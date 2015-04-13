cb50ea4563c59109681ac3dbfb7d1cc67daeb09c
TGuillerme <guillert@tcd.ie>
2015-04-13 12:35:26 +0100

    v0.2 updated RAM friendly version

--- Functions/Cluster/disparity.null.sh
+++ Functions/Cluster/disparity.null.sh
@@ -11,11 +11,13 @@
 #<tree> the path to the phylogenetic tree (can be relative)
 #<disparity> the disparity metric
 #<type> either random or sim.char
-#<iterations> a number of iterations
+#<intervals> a series of time intervals
+#<slices> a series of time slices
 #########################
-#version 0.1
+#version 0.2
+#Update: RAM friendly version
 #----
-#guillert(at)tcd.ie - 31/03/2015
+#guillert(at)tcd.ie - 13/04/2015
 ###########################
 
 chain=$1
@@ -24,13 +26,15 @@ matrix=$3
 tree=$4
 disparity=$5
 type=$6
+intervals=$7
+slices=$8
 
 ######################################
 #R template
 ######################################
 echo "
 #Load the functions and the packages
-source('functions.R')
+library(disparity)
 
 ###################
 #Reading the files
@@ -41,6 +45,8 @@ chain_name='${chain}'
 data_path='${path}'
 file_matrix='${matrix}'
 file_tree='${tree}'
+intervals=as.numeric(strsplit(c(noquote('${intervals}')), split=',')[[1]])
+slices=as.numeric(strsplit(c(noquote('${slices}')), split=',')[[1]])
 
 #matrix
 Nexus_data<-ReadMorphNexus(file_matrix)
@@ -70,37 +76,88 @@ tree<-bin.tree(tree)
 tree<-lapply.root(tree, max(tree.age(tree)\$age))
 
 ######################################
-#Calculating null disparity
+#Calculating null disparity 
 ######################################
 
 rand_data<-null.data(tree=tree, matrix=apply(Nexus_data\$matrix, 2, states.count), matrix.model='${type}', replicates=<REPLICATES>, verbose=TRUE, include.nodes=TRUE)
 ran_matrix<-lapply(rand_data, make.nexus)
 #distance
 dist_ran<-lapply(ran_matrix, MorphDistMatrix.verbose, verbose=TRUE)
-dist_ran<-lapply(dist_ran, extract.dist, distance='max.dist.matrix')
+dist_ran<-lapply(dist_ran, extract.dist, distance='gower.dist.matrix')
 #pco
-pco_ran<-lapply(dist_ran, function(X) cmdscale(X, k=nrow(X)-1, add=TRUE)\$points)
+pco_data<-lapply(dist_ran, function(X) cmdscale(X, k=nrow(X)-1, add=TRUE)\$points)
+
+######################################
 #intervals
-pco_ran_int<-lapply(pco_ran, function(X) int.pco(X, tree, intervals, include.nodes=TRUE))
+######################################
+
+#intervals
+pco_time<-lapply(pco_data, function(X) int.pco(X, tree, intervals, include.nodes=TRUE))
 #diversity
-div_ran_int<-int.pco(pco_ran[[1]], tree, intervals, include.nodes=TRUE, diversity=TRUE)[[2]]
+div_data<-int.pco(pco_data[[1]], tree, intervals, include.nodes=TRUE, diversity=TRUE)[[2]]
 #disparity
-disp_ran_int<-lapply(pco_ran_int, time.disparity, verbose=TRUE, method='${disparity}', save.all=TRUE)
+dis_data<-lapply(pco_time, time.disparity, verbose=TRUE, method='${disparity}', save.all=TRUE)
 #Combine results
-disp_ran_int<-combine.disp(disp_ran_int)
-save(disp_ran_int, file=paste(data_path, chain_name, '/',chain_name,'-diparity_null_${type}_${disparity}<NAME>.Rda', sep=''))
+dis_data<-combine.disp(dis_data)
+#list for output
+disp_null_int<-list('diversity'=div_data, 'disparity'=dis_data)
+save(disp_null_int, file=paste(data_path, chain_name, '/',chain_name,'-diparity_null-int-${type}_${disparity}<NAME>.Rda', sep=''))
+
+#Saving RAM!
+pco_time<-NULL
+div_data<-NULL
+dis_data<-NULL
+disp_null_int<-NULL
+
+######################################
+#Slices
+######################################
+
+#slices - random
+pco_time<-lapply(pco_data, function(X) slice.pco(X, tree, slices, verbose=TRUE, method='random'))
+#diversity
+div_data<-slice.pco(pco_data[[1]], tree, slices, verbose=TRUE, method='random')[[2]]
+#disparity
+dis_data<-lapply(pco_time, time.disparity, verbose=TRUE, method='${disparity}', save.all=TRUE)
+#list for output
+disp_null_int<-list('diversity'=div_data, 'disparity'=dis_data)
+save(disp_null_int, file=paste(data_path, chain_name, '/',chain_name,'-diparity_null-sli_ran-${type}_${disparity}<NAME>.Rda', sep=''))
+
+#slices - acctran
+pco_time<-lapply(pco_data, function(X) slice.pco(X, tree, slices, verbose=TRUE, method='acctran'))
+#disparity
+dis_data<-lapply(pco_time, time.disparity, verbose=TRUE, method='${disparity}', save.all=TRUE)
+#list for output
+disp_null_int<-list('diversity'=div_data, 'disparity'=dis_data)
+save(disp_null_int, file=paste(data_path, chain_name, '/',chain_name,'-diparity_null-sli_acc-${type}_${disparity}<NAME>.Rda', sep=''))
+
+#slices - deltran
+pco_time<-lapply(pco_data, function(X) slice.pco(X, tree, slices, verbose=TRUE, method='deltran'))
+#disparity
+dis_data<-lapply(pco_time, time.disparity, verbose=TRUE, method='${disparity}', save.all=TRUE)
+#list for output
+disp_null_int<-list('diversity'=div_data, 'disparity'=dis_data)
+save(disp_null_int, file=paste(data_path, chain_name, '/',chain_name,'-diparity_null-sli_del-${type}_${disparity}<NAME>.Rda', sep=''))
+
+#slices - proximity
+pco_time<-lapply(pco_data, function(X) slice.pco(X, tree, slices, verbose=TRUE, method='proximity'))
+#disparity
+dis_data<-lapply(pco_time, time.disparity, verbose=TRUE, method='${disparity}', save.all=TRUE)
+#list for output
+disp_null_int<-list('diversity'=div_data, 'disparity'=dis_data)
+save(disp_null_int, file=paste(data_path, chain_name, '/',chain_name,'-diparity_null-sli_pro-${type}_${disparity}<NAME>.Rda', sep=''))
 " > disparity.null.template.tmp
 
 ######################################
 #Tasker
 ######################################
 
-for ((rep=1 ;  rep<=8 ;  rep+=1))
+for ((rep=1 ;  rep<=24 ;  rep+=1))
 do  
     #make the replicate files
-    sed 's/<REPLICATES>/13/g' disparity.null.template.tmp | sed 's/<NAME>/'"${rep}"'/g' > ${chain}-${type}_${disparity}.${rep}.R
+    sed 's/<REPLICATES>/5/g' disparity.null.template.tmp | sed 's/<NAME>/'"${rep}"'/g' > ${chain}-${type}_${disparity}.${rep}.R
     #make the shell files
-    echo "R --no-save < ${chain}-${type}_${disparity}.${rep}.R" > ${chain}-${type}_${disparity}.${rep}.sh
+    echo "R --no-save < ${chain}-${type}_${disparity}.${rep}.R > /dev/null" > ${chain}-${type}_${disparity}.${rep}.sh
     #making the config file
     n=$(( $rep - 1 )) 
     echo "$n sh ${chain}-${type}_${disparity}.${rep}.sh" >> ${chain}-${type}_${disparity}.config
@@ -110,7 +167,7 @@ done
 rm disparity.null.template.tmp
 
 echo "#!/bin/sh
-#SBATCH -n 8
+#SBATCH -n 24
 #SBATCH -t 4-00:00:00
 #SBATCH -p compute
 #SBATCH -J D-${chain}
