e53cdfea72956cc091c34c7e5662d3507f0f1bb1
TGuillerme <guillert@tcd.ie>
2015-08-17 16:03:50 +0100

    updated path

--- Analysis/Data_setups/Data_setup_Slater_claddis.R
+++ Analysis/Data_setups/Data_setup_Slater_claddis.R
@@ -1,19 +1,10 @@
 #Script for doing ancestral states reconstruction and distance matrix from an input nexus file
 
-#Setwd
-if(length(grep("TGuillerme", getwd()))) {
-    setwd('~/PhD/Projects/SpatioTemporal_Disparity/Analysis')
-} else {
-    warning("You might have to change the directory!")
-}
-if(length(grep("SpatioTemporal_Disparity/Analysis", getwd()))==0) {
-    if(length(grep("SpatioTemporal_Disparity-master/Analysis", getwd()))==0) {
-        stop("Wrong directory!\nThe current directory must be:\nSpatioTemporal_Disparity/Analysis/ OR SpatioTemporal_Disparity-master/Analysis/\nYou can clone the whole repository from:\nhttps://github.com/TGuillerme/SpatioTemporal_Disparity")
-    }
-}
+#Loading the package
+library(devtools)
+install_github("TGuillerme/SpatioTemporal_Disparity/Functions/disparity")
+library(disparity)
 
-#Load the functions and the packages
-source("functions.R")
 
 ###################
 #Reading the files
