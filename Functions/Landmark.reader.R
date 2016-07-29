ca48ff14ad4edb2f71c08ea66aa429f9742a7000
Thomas Guillerme <guillert@tcd.ie>
2016-07-29 13:28:24 +0100

    Landmark reader updated

--- Functions/Landmark.reader.R
+++ Functions/Landmark.reader.R
@@ -61,9 +61,65 @@ trigonid_slides <- get.landmarks(raw_coordinates, number_specimen, 49, c(29,48))
 # Adding the specimens names
 names(internal_landmarks) <- names(talonid_slides) <- names(trigonid_slides) <- species_names
 
-# Remove the coordinates with only 0s
+# Reorient all the landmarks and curves
+# The first landmark (paraconid) must have the lowest x value
+# The sixth landmark (hypoconulid) must have the highest x value
 
 
+# for(which_outline in 1:length(internal_landmarks)) {
+
+#     one_landmark_set <- internal_landmarks[[which_outline]]
+#     #Set the detection range
+#     detection_x <- range(one_landmark_set[,1])[2] - range(one_landmark_set[,1])[1]
+#     detection_x <- detection_x - 0.1*detection_x #with 10% tolerance
+#     detection_y <- range(one_landmark_set[,2])[2] - range(one_landmark_set[,2])[1]
+#     detection_y <- detection_y - 0.1*detection_y #with 10% tolerance
+
+#     if(one_landmark_set[6,1] < one_landmark_set[1,1] && abs(one_landmark_set[1,1]-one_landmark_set[6,1]) > detection_x) {
+#         internal_landmarks[[which_outline]] <- rotate.coordinates(internal_landmarks[[which_outline]], mirror = "horizontal")
+#         talonid_slides[[which_outline]] <- rotate.coordinates(talonid_slides[[which_outline]], mirror = "horizontal")
+#         trigonid_slides[[which_outline]] <- rotate.coordinates(trigonid_slides[[which_outline]], mirror = "horizontal")
+#     } else {
+#         if(one_landmark_set[6,2] < one_landmark_set[1,2] && abs(one_landmark_set[6,2]-one_landmark_set[1,2]) > detection_y) {
+#             internal_landmarks[[which_outline]] <- rotate.coordinates(internal_landmarks[[which_outline]], rotation = 270)
+#             talonid_slides[[which_outline]] <- rotate.coordinates(talonid_slides[[which_outline]], rotation = 270)
+#             trigonid_slides[[which_outline]] <- rotate.coordinates(trigonid_slides[[which_outline]], rotation = 270)
+#         } else {
+#             if(one_landmark_set[6,2] > one_landmark_set[1,2] && abs(one_landmark_set[6,2]-one_landmark_set[1,2]) > detection_y) {
+#                 internal_landmarks[[which_outline]] <- rotate.coordinates(internal_landmarks[[which_outline]], rotation = 90)
+#                 talonid_slides[[which_outline]] <- rotate.coordinates(talonid_slides[[which_outline]], rotation = 90)
+#                 trigonid_slides[[which_outline]] <- rotate.coordinates(trigonid_slides[[which_outline]], rotation = 90)
+#             }
+#         }
+#     }
+# }
+
+rotate.coordinates <- function(coordinates, rotation, mirror) {
+
+    # warning("proportions not preserved!")
+    rotated_coords <- coordinates
+
+    if(!missing(rotation) && rotation != 0) {
+        # rotation_matrix <- matrix(c(cos(rotation), -sin(rotation), sin(rotation), cos(rotation)), 2, 2)
+        # rotated_coords <- coordinates %*% rotation_matrix
+
+        rotated_coords[,1] = coordinates[,1] * cos(rotation) - coordinates[,2] * sin(rotation)
+        rotated_coords[,2] = coordinates[,2] * cos(rotation) + coordinates[,1] * sin(rotation)
+        # x' = x cos f - y sin f
+        # y' = y cos f + x sin f
+
+    }
+    if(!missing(mirror)) {
+        if(mirror == "vertical") {
+            rotated_coords[,1] <- rotated_coords[,1]*-1
+        }
+        if(mirror == "horizontal") {
+            rotated_coords[,2] <- rotated_coords[,2]*-1
+        }
+    }
+
+    return(rotated_coords)
+}
 
 
 # Plotting the teeth!
@@ -93,34 +149,32 @@ for(i in 1:35) {
     plot.teeth(i, talonid_slides, trigonid_slides, internal_landmarks, legend = FALSE, axis = FALSE)
 }
 
-rotate.coordinates <- function(coordinates, rotation, mirror) {
 
-    warning("proportions not preserved!")
-    rotated_coords <- coordinates
+# plot(rotate.coordinates(bla, mirror = "vertical"), type = "l")
 
-    if(!missing(rotation) && rotation != 0) {
-        # rotation_matrix <- matrix(c(cos(rotation), -sin(rotation), sin(rotation), cos(rotation)), 2, 2)
-        # rotated_coords <- coordinates %*% rotation_matrix
 
-        rotated_coords[,1] = coordinates[,1] * cos(rotation) - coordinates[,2] * sin(rotation)
-        rotated_coords[,2] = coordinates[,2] * cos(rotation) + coordinates[,1] * sin(rotation)
-        # x' = x cos f - y sin f
-        # y' = y cos f + x sin f
+# Combining the talonid and trigonid slides
+combined_slides <- mapply(rbind, talonid_slides, trigonid_slides, SIMPLIFY = FALSE)
+# Combining all the data
+slides_n_landmarks <- mapply(rbind, internal_landmarks, combined_slides, SIMPLIFY = FALSE)
 
-    }
-    if(!missing(mirror)) {
-        if(mirror == "vertical") {
-            rotated_coords[,1] <- rotated_coords[,1]*-1
-        }
-        if(mirror == "horizontal") {
-            rotated_coords[,2] <- rotated_coords[,2]*-1
-        }
-    }
-
-    return(rotated_coords)
+# Set data as an array
+list.to.array <- function(list) {
+    return(array(unlist(list), dim = c(nrow(list[[1]]), ncol(list[[1]]), length(list))))
 }
 
-plot(rotate.coordinates(bla, mirror = "vertical"), type = "l")
+# Setting the different arrays
+internal_landmarks_array <- list.to.array(internal_landmarks)
+combined_slides_array <- list.to.array(combined_slides)
+slides_n_landmarks_array <- list.to.array(slides_n_landmarks)
+
+# Curves data for procrustes analysis
+
+# Procrustes analysis
+library(geomorph)
+proc_internal_landmarks <- gpagen(internal_landmarks_array)
+
+ordination <- prcomp(proc_internal_landmarks$data)$x
 
 #######
 # 2D Geomorph
